1fdfde0d812291f98a7edeebc1e9795a8453622c
Gerry Weißbach <gerry.w@gammaproduction.de>
2015-07-23 09:51:50 +0200

    Initial Commit

---
+++ SetupBuilderOSXPrefPane/SetupBuilderOSXPrefPane.h
@@ -0,0 +1,15 @@
+//
+//  SetupBuilderOSXPrefPane.h
+//  SetupBuilderOSXPrefPane
+//
+//  Created by Gerry Weißbach on 23/07/2015.
+//  Copyright (c) 2015 i-net software. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface SetupBuilderOSXPrefPane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ SetupBuilderOSXPrefPane/SetupBuilderOSXPrefPane.m
@@ -0,0 +1,17 @@
+//
+//  SetupBuilderOSXPrefPane.m
+//  SetupBuilderOSXPrefPane
+//
+//  Created by Gerry Weißbach on 23/07/2015.
+//  Copyright (c) 2015 i-net software. All rights reserved.
+//
+
+#import "SetupBuilderOSXPrefPane.h"
+
+@implementation SetupBuilderOSXPrefPane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
